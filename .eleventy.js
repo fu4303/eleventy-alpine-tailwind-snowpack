16f1998a3c7c6df0ed3735a23b65d1475d967cca
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-07-20 19:14:50 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -72,6 +72,8 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: 'src',
+      includes: '_includes',
+      data: '_data',
       output: '_site',
     },
     // passthroughFileCopy: true,
